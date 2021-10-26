1fbfeba0a075930d0257b2cef7bfb2e61556701e
vivek vardhan . M <84318895+vikkyvivek2002@users.noreply.github.com>
2021-10-26 22:56:50 +0530

    Add files via upload

---
+++ 23.py
@@ -0,0 +1,12 @@
+s2=int(input())
+if s2>=35:
+    if s2>=75:
+        print("telugu:",s2,"/100","p","A+")
+    elif s2>=65:
+        print("telugu:",s2,"/100","p","A")
+    elif s2>=55:
+        print("telugu:",s2,"/100","p","B")
+    elif s2>=35:
+        print("telugu:",s2,"/100","p","C")
+else:
+    print("telugu:",s2,"/100","f","f")
